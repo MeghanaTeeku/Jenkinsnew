eb61b5fc85289fb2a07dbe303ae625c5e6278e07
MeghanaTeeku <Meghanarteeku@gmail.com>
2024-04-20 10:28:22 +0530

    firstcommit

---
+++ src/test/java/Sample1Test.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+public class Sample1Test {
+
+	@Test
+	public void method1() {
+		System.out.println("Method1");
+	}
+	
+	@Test
+	public void method2() {
+		System.out.println("Method2");
+	}
+	
+	@Test
+	public void method3() {
+		System.out.println("Method3");
+	}
+}
---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class SampleTest {
+@Test
+public void method1() {
+	System.out.println("Method1 is running");
+}
+@Test
+public void method2() {
+	System.out.println("method2");
+}
+@Test
+public void method3() {
+	System.out.println("Method3");
+}
+}
